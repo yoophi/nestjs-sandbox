20e3d585ccca48ff4d48fc1a9605f98a2eb855c0
Pyunghyuk Yoo <yoophi@gmail.com>
2022-05-29 22:29:51 +0900

    :sparkles: UPDATE: add ApiResponse

--- todo-app-with-swagger/src/todos/todos.controller.ts
+++ todo-app-with-swagger/src/todos/todos.controller.ts
@@ -1,34 +1,48 @@
-import { Controller, Get, Post, Patch, Delete } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @ApiTags('Todo')
 @Controller('todos')
 export class TodosController {
   @ApiOperation({ summary: 'get Todo list' })
+  @ApiOkResponse()
   @Get()
   getTodoList() {
     return [];
   }
 
   @ApiOperation({ summary: 'create new Todo' })
+  @ApiOkResponse()
+  @ApiCreatedResponse()
+  @ApiBadRequestResponse()
   @Post()
   createTodo() {
     return [];
   }
 
   @ApiOperation({ summary: 'get Todo Detail' })
+  @ApiOkResponse()
   @Get('/:todoId')
   getTodoDetail() {
     return {};
   }
 
   @ApiOperation({ summary: 'update Todo' })
+  @ApiOkResponse()
+  @ApiBadRequestResponse()
   @Patch('/:todoId')
   updateTodo() {
     return {};
   }
 
   @ApiOperation({ summary: 'delete Todo' })
+  @ApiOkResponse()
   @Delete('/:todoId')
   deleteTodo() {
     return {};
