da6eeb0fbfe333a0b4674e5ee16921dde9ca85b1
Pyunghyuk Yoo <yoophi@gmail.com>
2022-05-29 22:03:24 +0900

    :sparkles: NEW: apply SwaggerModule

--- todo-app-with-swagger/src/main.ts
+++ todo-app-with-swagger/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Swagger Example')
+    .setDescription('Swagger Test')
+    .setVersion('0.1.0')
+    .addTag('Todo')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
