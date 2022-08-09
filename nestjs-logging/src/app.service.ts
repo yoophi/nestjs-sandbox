5523d4d14f19a604f80b461f72a293e8a709795b
Pyunghyuk Yoo <yoophi@gmail.com>
2022-08-10 00:18:57 +0900

    ✨ ADD: logging samples

--- nestjs-logging/src/app.service.ts
+++ nestjs-logging/src/app.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name);
+
   getHello(): string {
+    this.logger.log('Hello, World!');
+    this.logger.debug({ message: 'Hello, World!' });
+    this.logger.verbose('Hello, World! with verbose');
+    this.logger.warn('Hello, World! with warn');
     return 'Hello World!';
   }
 }
